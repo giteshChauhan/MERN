21fa90bda6cf09f4b6e52d7bc00f0558417f9284
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-06-09 13:53:50 +0530

    added genrerateAuthToken function

--- project_vidly/models/users.js
+++ project_vidly/models/users.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const passwordComplexity = require('joi-password-complexity');
 const mongoose = require('mongoose');
 const Joi = require('joi');
@@ -23,6 +25,11 @@ const userSchema = new mongoose.Schema({
         maxlength: 1024
     }
 });
+
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id},config.get('jwtPrivateKey'));
+    return token;
+}
 
 const Users = mongoose.model('user',userSchema);
 
--- project_vidly/routes/auth.js
+++ project_vidly/routes/auth.js
@@ -1,4 +1,3 @@
-const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const express = require('express');
 const Joi = require('joi');
@@ -15,7 +14,7 @@ router.post('/',async (req,res) => {
     const validPassword = await bcrypt.compare(req.body.password,user.password);
     if(!validPassword) return res.status(400).send('Invalid email or password');
     
-    const token = jwt.sign({_id: user._id}, 'jwtTokenPrivateKey');
+    const token = user.generateAuthToken();
     res.send(token);
 });
 
