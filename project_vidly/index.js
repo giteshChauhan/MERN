f9312441c0d59e7738de0527cd65d6b0d480ad7d
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-06-11 02:07:21 +0530

    restructured index.js

--- project_vidly/index.js
+++ project_vidly/index.js
@@ -1,53 +1,12 @@
-require('express-async-errors');
 const winston = require('winston');
-const config = require('config');
 const express = require('express');
-const mongoose = require('mongoose');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const login = require('./routes/login');
-const auth = require('./middleware/auth');
-const admin = require('./middleware/admin');
-const error = require('./middleware/error');
 const app = express();
 
-winston.add(new winston.transports.File({
-    filename: 'log/exceptions.log',
-    handleExceptions: true
-}));
-
-winston.add(new winston.transports.File({
-    filename: 'log/rejections.log',
-    handleRejections: true
-}));
-
-winston.add(new winston.transports.File({ filename: 'log/logfile.log' }));
-
-
-if(!config.get('jwtPrivateKey')){
-    console.error('FATAL ERROR: jwtPrivateKey is not defined');
-    process.exit(1);
-}
-
-mongoose.connect('mongodb://localhost/project_vidly')
-    .then(() => console.log('Connected to database...'))
-    .catch((err) => console.log('Could not connect to database..',err));
-
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use('/api/genres',[auth, admin],genres);
-app.use('/api/customers',customers);
-app.use('/api/movies',[auth, admin],movies);
-app.use('/api/rentals',auth,rentals);
-app.use('/api/users',users);
-app.use('/api/login',login);
-app.use(error);
-
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.port || 3000;
-app.listen(port,() => console.log(`Listening on port ${port}......`));
+app.listen(port,() => winston.info(`Listening on port ${port}......`));
