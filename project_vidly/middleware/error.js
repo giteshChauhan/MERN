741139063dbec8e1eb04f8ab98f09f2bfefe3d05
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-06-11 23:54:40 +0530

    updated logger method

--- project_vidly/middleware/error.js
+++ project_vidly/middleware/error.js
@@ -1,7 +1,7 @@
-const winston = require('winston');
+const logger = require('../startup/logging');
 
 module.exports = function(err,req,res,next){
-    winston.error(err.message, err);
+    logger.error(err.message);
 
     res.send(500).send('Something failed.');
 }
