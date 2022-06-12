e36d758b0774f8291329ec62f46d09f8c2048407
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-06-12 17:34:54 +0530

    updated db path

--- project_vidly/startup/db.js
+++ project_vidly/startup/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const {logger} = require('../logger/');
+const config = require('config');
 
 
 module.exports = function (){
-    mongoose.connect('mongodb://localhost/project_vidly')
+    mongoose.connect(config.get('db'));
     .then(() => logger.info('Connected to database...'));
 }
