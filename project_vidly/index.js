09a46dc66935d70a9c0db844ae97c78f275e49ca
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-06-12 17:37:33 +0530

    added app.use for prod.js

--- project_vidly/index.js
+++ project_vidly/index.js
@@ -7,6 +7,7 @@ require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 const port = process.env.port || 3000;
 app.listen(port,() => logger.info(`Listening on port ${port}......`));
