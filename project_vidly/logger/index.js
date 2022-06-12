6c89f5944022fe52c4ab043add7644567bb5c504
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-06-12 17:33:58 +0530

    changed prod logger path

--- project_vidly/logger/index.js
+++ project_vidly/logger/index.js
@@ -1,9 +1,9 @@
-const devLogger = require('./devLogger');
+const prodLogger = require('./prodLogger');
 const xlogger = require('./exceptionLogger');
 const rlogger = require('./rejectionLogger');
 
-// by default logger will export devLogger
-let logger = devLogger;
+// by default logger will export prodLogger
+let logger =prodLogger;
 
 
 module.exports.logger = logger;
