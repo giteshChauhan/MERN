cda1c9d6482945274ab1c92d410fb3312c8603fa
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-06-12 16:56:30 +0530

    updated logger path

--- project_vidly/index.js
+++ project_vidly/index.js
@@ -1,12 +1,12 @@
-const winston = require('winston');
+const {logger} = require('./logger/');
 const express = require('express');
 const app = express();
 
-require('./startup/logging')();
+require('./startup/handler')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
 
 const port = process.env.port || 3000;
-app.listen(port,() => winston.info(`Listening on port ${port}......`));
+app.listen(port,() => logger.info(`Listening on port ${port}......`));
